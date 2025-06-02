b2b006330cd676b8c90935226f1e80749b3ff2c6
Jon <jonwlester@gmail.com>
2025-06-02 08:51:22 +0100

    bug fix

--- api/state.js
+++ api/state.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
         const state = await kv.get('itinerary-state');
         res.status(200).json(state || {});
     } else if (req.method === 'POST') {
-        const data = await req.json();
+        const data = req.body;
         await kv.set('itinerary-state', data);
         res.status(200).json({ ok: true });
     } else {
